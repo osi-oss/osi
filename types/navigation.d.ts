1bdd7a93f195bf3ed72a55da1802cb79f18b9cde
Alexander Dorofeev <66268316+dorofeevalexru@users.noreply.github.com>
2026-02-05 14:04:31 +0300

    Delete types directory

--- types/navigation.d.ts
+++
@@ -1,8 +0,0 @@
-// src/types/navigation.d.ts
-import { RootStackParamList } from '@/navigation/AppNavigator';
-
-declare global {
-  namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
-  }
-}
